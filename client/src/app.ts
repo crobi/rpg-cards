e2aa2cd4a1bb6246b02ecd83db74d6b5d3c61d48
crobi <robin@dbservice.com>
2015-07-04 21:06:15 +0200

    Add global debugging functions
    to be used from the browser console

--- client/src/app.ts
+++ client/src/app.ts
@@ -5,10 +5,24 @@
 
 module rpgcards {
 
+    // Global objects, only accessible within this file
     var appDispatcher: Dispatcher = null;
     var appActions: Actions = null;
     var appStore: Store = null;
 
+    // Access to the global objects, to be used from the browser debug window
+    export function debug() {
+        return {
+            dispatcher: appDispatcher,
+            actions: appActions,
+            store: appStore,
+            refresh: refresh,
+            testState: setupTestState
+        }
+    }
+
+    // Initializes the application
+    // Creates and links the global objects
     export function bootstrap() {
         appDispatcher = new Dispatcher();
         appActions = new Actions(appDispatcher);
@@ -18,6 +32,9 @@ module rpgcards {
         refresh();
     }
 
+    // Renders the whole application
+    // In this project, render*() functions are pure functions that map
+    // the application state (Store) to React elements
     function renderApp(store: Store) {
         return React.DOM.div(
             {},
@@ -26,8 +43,23 @@ module rpgcards {
             );
     }
 
-    export function refresh() {
+    function refresh() {
         React.render(renderApp(appStore), document.body);
+    }
+
+    function setupTestState() {
+        console.log(
+            "This function resets the current application state"
+            + " and then triggers a series of hardcoded user actions,"
+            + " in order to set up some application state that can be used"
+            + " for testing");
+        appActions.reset();
+        appActions.newDeck();
+        appStore.getDeckList().lift(deckIds => appActions.newCard(deckIds[0]));
+        appStore.getDeckList().lift(deckIds => appActions.newCard(deckIds[0]));
+        appActions.newDeck();
+        appStore.getDeckList().lift(deckIds => appActions.newCard(deckIds[1]));
+        appActions.newDeck();
     }
 
 }
