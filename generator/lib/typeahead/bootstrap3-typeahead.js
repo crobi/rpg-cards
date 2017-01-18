fc0a6c77cd112aa3e6349f62945ef6c1e87f587f
Colin Kierans <kierans@monstercat.com>
2017-01-18 15:46:02 -0800

    Added a small icon to the dropdown typeahead

--- generator/lib/typeahead/bootstrap3-typeahead.js
+++ generator/lib/typeahead/bootstrap3-typeahead.js
@@ -204,6 +204,8 @@
       items = $(items).map(function (i, item) {
         i = $(that.options.item).data('value', item);
         i.find('a').html(that.highlighter(item));
+        var classname = 'icon-' + item.split(' ').join('-').toLowerCase()
+        i.find('a').append('<span class="' + classname + '"></span>')
         return i[0];
       });
 
