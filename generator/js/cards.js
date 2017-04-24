cba88580a881aa78de4c64853e9db7f43844c40c
Robert Autenrieth <robin@dbservice.com>
2017-04-24 07:22:38 +0200

    tweak icon alignment

--- generator/js/cards.js
+++ generator/js/cards.js
@@ -113,15 +113,8 @@ function card_element_inline_icon(params, card_data, options) {
     var icon = params[0] || "";
     var size = params[1] || "40";
     var align = params[2] || "center";
-    var margin_left = 0;
     var color = card_data_color_front(card_data, options);
-    if (align === "center") {
-        margin_left = (size/-2) + 'px';
-    }
-    else if (align === 'right') {
-        margin_left = 'auto';
-    }
-    return '<div class="card-element card-inline-icon align-' + align + ' icon-' + icon + '" style ="height:' + size + 'px; width: ' + size + 'px; margin-left: ' + margin_left + '; background-color: ' + color + '"></div>';
+    return '<div class="card-element card-inline-icon align-' + align + ' icon-' + icon + '" style ="height:' + size + 'px;min-height:' + size + 'px;width: ' + size + 'px;background-color: ' + color + '"></div>';
 }
 
 function card_element_picture(params, card_data, options) {
