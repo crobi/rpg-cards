654e3b19d1da04b3abd154b922a85a96982fbc72
Jure di Mephit <www.mephit.it@gmail.com>
2024-04-23 01:54:16 +0200

    Update output.js
    output wait for body

--- generator/js/output.js
+++ generator/js/output.js
@@ -5,18 +5,25 @@ function receiveMessage(event) {
 
 function insertCards(html) {
     // Remove all previous content
-    while (document.body.hasChildNodes()) {
-        document.body.removeChild(document.body.lastChild);
-    }
-
-    // Create a div that holds all the received HTML
-    var div = document.createElement("div");
-    div.setAttribute("class", "output-container");
-    div.id = "output-container";
-    div.innerHTML = html;
-
-    // Add the new div to the document
-    document.body.appendChild(div);
+    (function waitForBody() {
+        console.log(document.body);
+        if (!document.body){
+            requestAnimationFrame(waitForBody);
+            return;
+        }
+        while (document.body.hasChildNodes()) {
+            document.body.removeChild(document.body.lastChild);
+        }
+    
+        // Create a div that holds all the received HTML
+        var div = document.createElement("div");
+        div.setAttribute("class", "output-container");
+        div.id = "output-container";
+        div.innerHTML = html;
+    
+        // Add the new div to the document
+        document.body.appendChild(div);
+    })();
 }
 
 window.addEventListener("message", receiveMessage, false);
